1d7d1ea22cd7d0986731df5faa94f1f9af533254
cobra <king.co6ra@yandex.ru>
2022-06-12 12:35:36 +0300

    test big begin port

--- tests.py
+++ tests.py
@@ -30,3 +30,9 @@ class ScannerPointTestCase(AioHTTPTestCase):
             self.assertEqual(response.status, 400)
             answer = await response.text()
             self.assertIn('400 bad request: illegal IP address string passed to inet_aton', answer)
+
+    async def test_big_begin_port(self):
+        async with self.client.request('GET', '/scan/185.215.4.66/8000/1') as response:
+            self.assertEqual(response.status, 400)
+            answer = await response.text()
+            self.assertIn('400 bad request: begin_port cannot be greater than end_port and less than zero', answer)
